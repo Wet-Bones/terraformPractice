c899563faf53bd2ebd013843449349bd3b8ffeac
Nathaniel Bogaty <nathanielbogaty@gmail.com>
2024-10-03 12:59:22 -0700

    main init

---
+++ subnet/Subnet-main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_resource_group" "rg01" {
+	name = var.rg_name
+	location = "East US"
+}
+
+resource "azurerm_subnet" "example" {
+	#count = length(var.subnet_name)
+	name			= var.subnet_name
+	resource_group_name	= resource.azurerm_resource_group.rg01.name
+	virtual_network_name	= var.vnet-name
+	address_prefixes	= var.subnet_CIDR
+}
+
+output "sub_id" {
+	value = azurerm_subnet.example.id
+}
